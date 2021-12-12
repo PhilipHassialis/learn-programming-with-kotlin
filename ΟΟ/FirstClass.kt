a34dbf5df27e171c7cf3a2705197d072a7ce547e
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-12-12 19:36:33 +0200

    open classes and inheritance

--- ΟΟ/FirstClass.kt
+++ ΟΟ/FirstClass.kt
@@ -2,12 +2,13 @@ package OO
 
 import java.time.LocalDate
 
-class Person(var name: String, var age: Int) {
+open class Person(open val name: String, open var age: Int) {
 
-    var birthYear = LocalDate.now().year - age
+    var birthYear: Int
 
     init {
         println("Object was created")
+        birthYear = LocalDate.now().year - age
     }
 
     fun speak() {
@@ -23,13 +24,35 @@ class Person(var name: String, var age: Int) {
     // }
 
     // fun getYearOfBirth(): Int = LocalDate.now().year - age
-    fun getYearOfBirth(): Int = birthYear
+    fun getYearOfBirth(): Int = LocalDate.now().year - age
+}
+
+class Student(override val name: String, override var age: Int, val studentId: String) :
+        Person(name, age) {
+    fun isIntelligent() = true
+}
+
+class Employee(override val name: String, override var age: Int) : Person(name, age) {
+    fun receivePayment() {
+        println("Received payment")
+    }
 }
 
 fun main() {
-    val person = Person("Philip Alexander", 45)
-    person.speak()
-    println("name is ${person.name}")
-    person.greet("World")
-    println("year of birth is ${person.getYearOfBirth()}")
+    // val person = Person("Philip Alexander", 45)
+    // person.speak()
+    // println("name is ${person.name}")
+    // person.greet("World")
+    // println("year of birth is ${person.getYearOfBirth()}")
+
+    val student = Student("Philip", 45, "1234")
+    student.speak()
+    student.greet("Alex")
+
+    println(student.isIntelligent())
+
+    val employee = Employee("Helen", 45)
+    println(employee.getYearOfBirth())
+
+    employee.receivePayment()
 }
