fa44cdb674e7a9ee3c0ba113ad5a7040c7301ae7
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-12-28 07:49:17 +0200

    Binary numbers and colors enum

--- OO/CarColors.kt
+++ OO/CarColors.kt
@@ -4,7 +4,11 @@ public enum class CarColors(val rgb: Int) {
     RED(0xFF0000),
     GREEN(0x00FF00),
     BLUE(0x0000FF),
-    YELLOW(0xFFFF00)
+    YELLOW(0xFFFF00);
+
+    fun containsRed(): Boolean {
+        return this.rgb and 0xFF0000 != 0
+    }
 }
 
 fun main() {
@@ -15,4 +19,18 @@ fun main() {
     println(0x000000) // black
     println(0xFFFFFF) // white
     println(0xFF0000) // red
+
+    println(0b1010)
+    println(0b11111111 == 0xFF)
+    println(0b11111111_00000000_00000000 == 0xFF0000)
+
+    println(0b1101 and 0b1011)
+
+    var cc = CarColors.GREEN
+    println(cc.containsRed())
+
+    var cc2 = CarColors.YELLOW
+    println(cc.containsRed())
+
+    println(CarColors.YELLOW.containsRed())
 }
