29c7d165238937c3afcb07f42d62d7d057f69e35
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-12-28 07:38:48 +0200

    Hexadecimal numbers

--- OO/CarColors.kt
+++ OO/CarColors.kt
@@ -1,7 +1,18 @@
 package OO
 
-public enum class CarColors {
-    RED,
-    GREEN,
-    BLUE
+public enum class CarColors(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00)
+}
+
+fun main() {
+    println(0x10)
+    println(0x1F)
+    println(0xDEADBEEF)
+
+    println(0x000000) // black
+    println(0xFFFFFF) // white
+    println(0xFF0000) // red
 }
