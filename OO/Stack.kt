4fcdf5d17be6c37243948e326e7632a7c198c34c
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-12-28 19:33:41 +0200

    Generic functions

--- OO/Stack.kt
+++ OO/Stack.kt
@@ -18,10 +18,20 @@ open class Stack<E>(vararg val items: E) {
     }
 }
 
+fun <T> stackOf(vararg elements: T): Stack<T> { // <T> functionname -> generic function of T
+    return Stack(*elements) // * -> spread operator
+}
+
 fun main() {
     val myStack = Stack(12, 3, 56, 7, 8)
     myStack.push(13)
     myStack.push(22)
 
+    val stack2 = stackOf("Oi", "Hey", "Hola")
+
     println(myStack.pop())
+
+    for (i in 1..3) {
+        println(stack2.pop())
+    }
 }
