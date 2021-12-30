6968bcc90996bc6d4974eba4cd624e2336d48159
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-12-30 08:50:55 +0200

    A little console game

---
+++ IO/NumberGuessing.kt
@@ -0,0 +1,25 @@
+package IO
+
+import java.util.Random
+
+fun main() {
+    val number = Random().nextInt(100) + 1
+    var input: String?
+    var guess = -1
+
+    while (guess != number) {
+        print("Guess a number between 1 and 100:")
+        input = readLine()
+        if (input != null) {
+            // can throw number format exception
+            guess = input.toInt()
+        }
+        if (guess < number) {
+            println("Too low")
+        } else if (guess > number) {
+            println("Too high")
+        } else {
+            println("You won!")
+        }
+    }
+}
